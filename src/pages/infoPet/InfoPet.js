246a693c60366933574f5e6806d58481e16c4f87
renan kanu <kanu@renans-MacBook-Pro.local>
2020-05-25 13:20:48 -0300

    [NPM] Add lib for share item

--- src/pages/infoPet/InfoPet.js
+++ src/pages/infoPet/InfoPet.js
@@ -8,11 +8,13 @@ import {
   Dimensions,
   ScrollView,
   StatusBar,
+  Platform,
 } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {sanFranciscoWeights} from 'react-native-typography';
 import LinearGradient from 'react-native-linear-gradient';
+import Share from 'react-native-share';
 import * as Animatable from 'react-native-animatable';
 import {colors} from '../../styles/commons-styles';
 import {useNavigation} from '@react-navigation/native';
@@ -21,6 +23,61 @@ import fonts from '../../styles/fonts';
 import owner from '../../assets/images/owner.jpeg';
 
 const {height, width} = Dimensions.get('window');
+const url = 'https://www.gatherpets.com/';
+const title = 'Gather Pets';
+const message = 'Please check this out.';
+const icon = 'data:<data_type>/<file_extension>;base64,<base64_data>';
+const options = Platform.select({
+  ios: {
+    activityItemSources: [
+      {
+        // For sharing url with custom title.
+        placeholderItem: {type: 'url', content: url},
+        item: {
+          default: {type: 'url', content: url},
+        },
+        subject: {
+          default: title,
+        },
+        linkMetadata: {originalUrl: url, url, title},
+      },
+      {
+        // For sharing text.
+        placeholderItem: {type: 'text', content: message},
+        item: {
+          default: {type: 'text', content: message},
+          message: null, // Specify no text to share via Messages app.
+        },
+        linkMetadata: {
+          // For showing app icon on share preview.
+          title: message,
+        },
+      },
+      {
+        // For using custom icon instead of default text icon at share preview when sharing with message.
+        placeholderItem: {
+          type: 'url',
+          content: icon,
+        },
+        item: {
+          default: {
+            type: 'text',
+            content: `${message} ${url}`,
+          },
+        },
+        linkMetadata: {
+          title: message,
+          icon: icon,
+        },
+      },
+    ],
+  },
+  default: {
+    title,
+    subject: title,
+    message: `${message} ${url}`,
+  },
+});
 
 export default function InfoPet({route}) {
   const {pet} = route.params;
@@ -31,7 +88,13 @@ export default function InfoPet({route}) {
   };
 
   const share = () => {
-    console.log('Share');
+    Share.open(options)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        err && console.log(err);
+      });
   };
 
   return (
