7266e98b92ea4b8937b2c1f95ccb95d9ddda11b6
renan kanu <kanu@renans-MacBook-Pro.local>
2020-05-26 17:49:16 -0300

    Final implements animations in tab bottom

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, {useState} from 'react';
+import {
+  StatusBar,
+  TouchableOpacity,
+  Animated,
+  View,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {
@@ -13,6 +21,7 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Feather from 'react-native-vector-icons/Feather';
+import {useSafeArea} from 'react-native-safe-area-context';
 
 import About from './pages/about/About';
 import ForgotPassword from './pages/forgotPassword/ForgotPassword';
@@ -29,6 +38,7 @@ import Notification from './pages/notification/Notification';
 import ChangePassword from './pages/myProfile/ChangePassword';
 import Chat from './pages/chat/Chat';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {colors} from './styles/commons-styles';
 
 const Stack = createStackNavigator();
 const store = createStore(rootReducer);
@@ -63,55 +73,147 @@ export default function Routes() {
     }),
   };
 
-  const HomeStackScreen = () => (
-    <Tab.Navigator
-      initialRouteName="Home"
-      activeColor="#36CA63"
-      inactiveColor="#fff"
-      shifting={true}
-      barStyle={{backgroundColor: '#293B83', paddingBottom: 12}}>
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({color}) => (
-            <Feather name="home" color={color} size={22} />
-          ),
-        }}
-      />
+  const BottomMenuItem = ({iconName, isCurrent}) => {
+    return (
+      <View style={styles.containerBottomMenu}>
+        <Feather
+          name={iconName}
+          size={24}
+          style={{color: isCurrent ? colors.yellowHeader : colors.primaryColor}}
+        />
+      </View>
+    );
+  };
 
-      <Tab.Screen
-        name="Delivery"
-        component={Login}
-        options={{
-          tabBarLabel: 'Doar',
-          tabBarIcon: ({color}) => (
-            <Feather name="plus-circle" color={color} size={22} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Chat"
-        component={Notification}
-        options={{
-          tabBarLabel: 'Chat',
-          tabBarIcon: ({color}) => (
-            <Feather name="message-square" color={color} size={22} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={Notification}
-        options={{
-          tabBarLabel: 'Notificações',
-          tabBarIcon: ({color}) => (
-            <Feather name="bell" color={color} size={22} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+  function MyTabBar({state, descriptors, navigation}) {
+    const [translateValue] = useState(new Animated.Value(0));
+    const totalWidth = Dimensions.get('window').width;
+    const tabWidth = totalWidth / state.routes.length;
+    return (
+      <View style={[styles.tabContainer, {width: totalWidth}]}>
+        <View style={{flexDirection: 'row'}}>
+          <Animated.View
+            style={[
+              styles.slider,
+              {
+                transform: [{translateX: translateValue}],
+                width: tabWidth - 40,
+              },
+            ]}
+          />
+          {state.routes.map((route, index) => {
+            const {options} = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                ? options.title
+                : route.name;
+
+            const isFocused = state.index === index;
+
+            const onPress = () => {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: route.key,
+                canPreventDefault: true,
+              });
+
+              if (!isFocused && !event.defaultPrevented) {
+                navigation.navigate(route.name);
+              }
+
+              Animated.spring(translateValue, {
+                toValue: index * tabWidth,
+                velocity: 10,
+                useNativeDriver: true,
+              }).start();
+            };
+
+            const onLongPress = () => {
+              navigation.emit({
+                type: 'tabLongPress',
+                target: route.key,
+              });
+            };
+
+            return (
+              <TouchableOpacity
+                accessibilityRole="button"
+                accessibilityStates={isFocused ? ['selected'] : []}
+                accessibilityLabel={options.tabBarAccessibilityLabel}
+                testID={options.tabBarTestID}
+                onPress={onPress}
+                onLongPress={onLongPress}
+                style={{flex: 1}}>
+                <BottomMenuItem
+                  iconName={label.toString()}
+                  isCurrent={isFocused}
+                />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
+
+  const HomeStackScreen = () => (
+    <View style={styles.containerHomeStack}>
+      <Tab.Navigator
+        initialRouteName="Home"
+        tabBar={(props) => <MyTabBar {...props} />}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: 'home',
+            tabBarIcon: ({color}) => (
+              <Feather name="home" color={color} size={22} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Delivery"
+          component={Login}
+          options={{
+            tabBarLabel: 'plus-circle',
+            tabBarIcon: ({color}) => (
+              <Feather name="plus-circle" color={color} size={22} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Chat"
+          component={Notification}
+          options={{
+            tabBarLabel: 'message-square',
+            tabBarIcon: ({color}) => (
+              <Feather name="message-square" color={color} size={22} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={Notification}
+          options={{
+            tabBarLabel: 'bell',
+            tabBarIcon: ({color}) => (
+              <Feather name="bell" color={color} size={22} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+      {useSafeArea().bottom > 0 && (
+        <View
+          style={{
+            height: useSafeArea().bottom - 5,
+            backgroundColor: colors.white,
+          }}
+        />
+      )}
+    </View>
   );
 
   return (
@@ -152,3 +254,39 @@ export default function Routes() {
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  tabContainer: {
+    height: 46,
+    shadowOffset: {
+      width: 0,
+      height: -1,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4.0,
+    backgroundColor: 'white',
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    elevation: 10,
+    position: 'absolute',
+    bottom: 0,
+  },
+  slider: {
+    height: 5,
+    position: 'absolute',
+    top: 0,
+    left: 20,
+    backgroundColor: colors.yellowHeader,
+    borderRadius: 10,
+    width: 50,
+  },
+  containerBottomMenu: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerHomeStack: {
+    flex: 1,
+    position: 'relative',
+  },
+});
