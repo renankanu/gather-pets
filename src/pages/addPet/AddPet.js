33089ed9d7c3f59d9eb62efc278203fe1cc97517
renan kanu <kanu@renans-MacBook-Pro.local>
2020-05-27 16:44:17 -0300

    Init add pet screen

--- src/pages/addPet/AddPet.js
+++ src/pages/addPet/AddPet.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import {View, Text, StatusBar, StyleSheet, Platform} from 'react-native';
+import {
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 import {useFocusEffect} from '@react-navigation/native';
-
+import Feather from 'react-native-vector-icons/Feather';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
+
 import {colors, commonsStyle} from '../../styles/commons-styles';
+import fonts from '../../styles/fonts';
 
 export default function AddPet() {
+  const navigation = useNavigation();
+
   useFocusEffect(
     React.useCallback(() => {
       StatusBar.setBarStyle('light-content');
@@ -14,21 +26,50 @@ export default function AddPet() {
     }, []),
   );
 
+  const comeBack = () => {
+    navigation.goBack();
+  };
+
   return (
     <SafeAreaView style={commonsStyle.backgroundApp}>
       <StatusBar
         barStyle="light-content"
         backgroundColor={colors.backgroundAppColor}
       />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={comeBack} style={styles.backButton}>
+          <Feather
+            name="chevron-left"
+            size={32}
+            color={colors.textPrimaryColor}
+          />
+        </TouchableOpacity>
+        <Text style={styles.title}>Adicionar animal</Text>
+      </View>
       <View style={styles.container}>
-        <Text>AddPet</Text>
+        <TouchableOpacity>
+          <Feather name="camera" size={32} color={colors.textPrimaryColor} />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
+    marginHorizontal: 20,
+  },
+  backButton: {
+    padding: 10,
+  },
+  title: {
+    fontSize: 32,
+    color: colors.textPrimaryColor,
+    fontFamily: fonts.BOLD,
   },
 });
