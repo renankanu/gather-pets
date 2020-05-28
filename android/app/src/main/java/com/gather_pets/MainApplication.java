c3d349a27c65c3d81727fd03c6a6bee6406db646
renan kanu <kanu@renans-MacBook-Pro.local>
2020-05-27 23:15:15 -0300

    Implements splah screen Android

--- android/app/src/main/java/com/gather_pets/MainApplication.java
+++ android/app/src/main/java/com/gather_pets/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.ijzerenhein.sharedelement.RNSharedElementPackage;
 import com.ijzerenhein.sharedelement.RNSharedElementPackage;
